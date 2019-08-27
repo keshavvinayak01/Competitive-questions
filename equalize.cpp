d4585aa4f5573ff765b7078792be9431c966a5d8
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-08-27 16:43:38 +0530

    Adhoc-1

---
+++ equalize.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    int cost = 0;
+    for(int i=0; i < n;){
+        if(s[i] != t[i]){
+            if(i+1 < n && s[i] != s[i+1] && s[i+1] != t[i+1]){
+                i += 2;
+            }
+            else{
+                i++;
+            }
+            cost++;
+        }
+        else{
+            i++;
+        }
+    } 
+
+    cout << cost << endl;
+    return 0;
+}
