cd028eb2bb3165d1daf1967313ed6756867c9288
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-02 16:46:55 +0530

    DP(15)

---
+++ min_sq_count.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCount(int n){
+    if (n <= 3) return n;
+    int* count = new int[n+1];
+    count[0] = 0; 
+    count[1] = 1;
+    count[2] = 2;
+    count[3] = 3;
+    for(int i = 4; i <=n ; i++){
+        int ans = i;
+        for(int j = 1; j <= i/2 ; j++){
+            int k = i - (j*j);
+            if(k >= 0)
+                ans = min(ans, count[k] + 1);
+        }
+        count[i] = ans;
+    }
+    return count[n];
+
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    int max = int(sqrt(n));
+    int *squares = new int[max];
+    cout << minCount(n) << endl;
+
+    return 0;
+}
