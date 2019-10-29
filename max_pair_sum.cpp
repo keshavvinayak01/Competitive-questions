33cac69ebbeb9bbf556bae23dfd8a4fd2d8fc679
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-29 11:43:06 +0530

    Max-pair-sum using segment Trees

--- max_pair_sum.cpp
+++ max_pair_sum.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int curr_idx = -1;
+
+struct node {
+    int value;
+    int idx;
+};
 
 void buildTree(int* arr, int* tree, int start, int end, int treeNode){
     if(start == end){
@@ -27,19 +33,32 @@ void updateTree(int *arr, int *tree, int start, int end, int treeNode, int idx, 
     
     tree[treeNode] = tree[2*treeNode] + tree[2*treeNode + 1];
 }
+node max_cmp(node a, node b){
+    return a.value > b.value ? a : b;
+}
 
-int query(int* tree, int start, int end, int treeNode, int left, int right) {
+node query(int* tree, int start, int end, int treeNode, int left, int right, node exclude) {
     // Completely outside range
-    if(start > right || end < left) return INT_MIN;
+    if(start > right || end < left || (start == end && treeNode == exclude.idx)){
+        node q;
+        q.value = INT_MIN;
+        q.idx = -1;
+        return q;
+    }
     
     // Completely inside
-    if(start >= left && end <= right) return tree[treeNode];
+    if(start == end){
+        node q;
+        q.value = tree[treeNode];
+        q.idx = treeNode;
+        return q;
+    }
     
     // Partially inside and partially outside
     int mid = (start + end)/2;
-    int ans1 = query(tree, start, mid, 2*treeNode, left, right);
-    int ans2 = query(tree, mid + 1, end, 2*treeNode + 1, left, right);
-    return max(ans1, ans2);
+    node ans1 = query(tree, start, mid, 2*treeNode, left, right, exclude);
+    node ans2 = query(tree, mid + 1, end, 2*treeNode + 1, left, right, exclude);
+    return max_cmp(ans1, ans2);
 }
 
 
@@ -49,20 +68,26 @@ int main(){
     int arr[n];
     for(int i = 0 ; i < n ; i++) cin >> arr[i];
     int* tree = new int[4*n];
+    buildTree(arr, tree, 0, n-1, 1);
     cin >> q;
     while(q--){
         char type;
         int a, b;
         cin >> type >> a >> b;
         if(type == 'U') {
-            updateTree(arr, tree, 0, n, 1, a-1, b);
+            updateTree(arr, tree, 0, n-1, 1, a-1, b);
         }
         else{
-            int mid = (a+b)/2;
-            cout << query(tree, 0, n-1, 1, a, mid) + query(tree, 0, n-1, 1, mid+1, b) << endl; 
+            a -= 1;
+            b -= 1;
+            node first_max;
+            first_max.value = INT_MIN; 
+            first_max.idx = -1;
+            first_max = query(tree, 0, n-1, 1, a, b, first_max);
+            node second_max = query(tree, 0, n-1, 1, a, b, first_max);
+            cout <<  first_max.value + second_max.value << endl; 
         }
     }
-    buildTree(arr, tree, 0, n, 1);
     
     return 0;
 }
