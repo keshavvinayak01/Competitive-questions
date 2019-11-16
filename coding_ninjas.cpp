bea813086cbc29e196ff06e5f67548aaef2277ea
Keshav VInayak Jha <keshavvinayakjha@gmail.com>
2019-11-17 03:53:42 +0530

    Correcteed Coding ninjas

--- coding_ninjas.cpp
+++ coding_ninjas.cpp
@@ -1,58 +1,51 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-void containString(int** edges, int n, int k, string main) {
-    n = n*k;
-    bool* visited = new bool[n];
-    queue<int> pendingVertices;
-    int start = -1;
-    pendingVertices.push(0);
-    visited[0] = true;
-
-    while(!pendingVertices.empty()) {
-        int currentVertex = pendingVertices.front();
-        pendingVertices.pop();  
-        visited[sv] = true;
-        cout << currentVertex << " ";
-        for(int i = 0; i < n; i++) {
-            if(i == currentVertex) continue;
-            if(edges[currentVertex][i] == 1 && !visited[i]) {
-                pendingVertices.push(i);
-                visited[i] = true;
-            }
-        }
-    }
-    delete[] visited;
-}
-                                                                        
-int main() {                                                                                    
-    int n, k;                                                                                   
-    cin >> n >> k;                                                                              
-    int** edges = new int*[n*k];
-    for(int i = 0 ; i < n*k; i++) edges[i] = new int[n*k];
-    string fullString = "";
-    string* graph = new string[n];
-
-    for(int i = 1; i <= n; i++) {
-        string s;
-        cin >> s;
-        graph[i] = s;
-        fullString += s;
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < k ; j++) {
-            edges[k*i + j][j+1] = edges[j+1][k*i + j] = 1;                 // right
-            edges[k*i + j][(k+1)*i + 1] = edges[(k+1)*i + 1][k*i + j] = 1; // bottom-right
-            edges[k*i + j][(k + 1)*i] = edges[(k + 1)*i][k*i + j] = 1;     // bottom
-            edges[k*i + j][(k+1)*i - 1] = edges[(k+1)*i - 1][k*i + j] = 1; // bottom-left
-            edges[k*i + j][j-1] = edges[j-1][k*i + j] = 1;                 // left
-            edges[k*i + j][(k-1)*i - 1] = edges[(k-1)*i - 1][k*i + j] = 1; // left-top
-            edges[k*i + j][(k-1)*i] = edges[(k-1)*i][k*i + j] = 1;         // top
-            edges[k*i + j][(k-1)*i + 1] = edges[(k-1)*i + 1][k*i + j] = 1; // right-top
-        }
-    }
-    string main = "CODINGNINJA";
+bool search2D( char Graph[][MAXN], int row, int col, string word, int nrow,int ncol, int level)
+{
+    int length = word.length();
     
-    cout << containString(edges, n, k, main) << endl;
-    return 0;
+    if( level == length)
+        return true;
+    
+    if( row < 0 || col < 0 || row >= nrow || col >= ncol)
+        return false;
+    
+    if( Graph[row][col] == word[level])
+    {
+        
+        char temp = Graph[row][col];
+        Graph[row][col] = '#';
+        
+        bool res =  search2D(Graph, row-1, col, word, nrow, ncol, level+1) |
+                    search2D(Graph, row+1, col, word, nrow, ncol, level+1) |
+                    search2D(Graph, row, col-1, word, nrow, ncol, level+1) |
+                    search2D(Graph, row, col+1, word, nrow, ncol, level+1) |
+                    search2D(Graph, row-1, col-1, word, nrow, ncol, level+1) |
+                    search2D(Graph, row-1, col+1, word, nrow, ncol, level+1) |
+                    search2D(Graph, row+1, col-1, word, nrow, ncol, level+1) |
+                    search2D(Graph, row+1, col+1, word, nrow, ncol, level+1);
+                    
+        Graph[row][col] = temp;
+        return res;
+    }
+    else
+        return false;
+}
+
+int solve(char Graph[][MAXN],int N, int M)
+{
+    string  word = "CODINGNINJA";
+	// Write your code here.
+    for( int row = 0 ; row < N; row++ )
+    {
+        for( int col = 0 ; col < M; col++ )
+        {
+            if( Graph[row][col] == word[0] )
+            {
+               if(search2D(Graph, row, col, word, N, M, 0) )
+                  return 1; 
+            }
+            
+        }
+    }
+        return 0;
+    
 }
