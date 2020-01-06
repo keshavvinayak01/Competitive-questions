f44d50dec94d7058d851e35976cd9902b2a3a4c9
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-06 12:23:01 +0530

    Fenwick Trees-1

--- query_distinct.cpp
+++ query_distinct.cpp
@@ -1,64 +1,89 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct event {
-    int first, second, index;
-};
+struct coder
+{
+	int first,second,index;
+}query[200000];
 
-bool compare(event a, event b) {
-    return a.second < b.second;
+bool operator < (coder A, coder B){
+	
+	return A.second < B.second;
 }
 
-void update(int index, int value, int n, int* bit) {
-    for(;index <= n; index += index&(-index)) {
-        bit[index] += value;
-    }
+int bit[30001];
+void update(int index,int value,int n){
+
+	for(;index<=n;index += index &(-index)){
+		bit[index] += value;
+	}
+}
+int last[1000001];
+int arr[30001];
+int value(int index){
+
+	int res= 0;
+	for(;index >0 ;index -= index & (-index)){
+		res += bit[index];
+	}
+	return res;
 }
 
-int value(int index, int* bit) {
-    int count = 0;
-    for(; index > 0; index -= index&(-index)) {
-        count += bit[index];
-    }
-    return count;
-}
+int main(){
+	int n;
+	cin >> n;
 
-int main() {
+	
+	for(int i=1;i<=n;i++){
+		cin >> arr[i];
+	}
 
-    int n;
-    cin >> n;
-    int* arr = new int[n+1];
-    for(int i = 1; i<= n; i++ ) cin >> arr[i];
+	int q;
+	cin>>q;
+	
+	for(int i=0;i<q;i++){
+		cin>>query[i].first>>query[i].second;
+		query[i].index = i;
+	}
 
-    int q;
-    cin >> q;
-    event* query = new event[q];
-    for(int i = 0; i < q ; i++) {
-        cin >> query[i].first >> query[i].second ;
-        query[i].index = i;      
-    }
-    sort(query, query + q, compare);
+	sort(query,query+q);
+	
 
-    int* bit = new int[n+1];
-    int* ans = new int[q];
-    int total = 0, k = 0;
-    int* last = new int[1000001];
-    for(int i = 0 ; i <= 1000000; i++) {
-        last[i] = -1;
-    }
-    for(int i=1; i<=q; i++) {
-        if(last[arr[i]] != -1) {
-            update(last[arr[i]], -1, n, bit);
-        }
-        else {
-            total++;
-        }
-        update(i, 1, n, bit);
-        last[arr[i]] = i;
-        while(k < q && query[k].second == i) {
-            ans[query[k].index] = total - value(query[k].first, bit);
-            k++;
-        }
-    }
-    return 0;
+
+	memset(last,-1,sizeof(last));
+
+
+
+	int k=0;
+	int ans[q];
+
+	
+	int total = 0;
+	for(int i=1;i<=n;i++){
+		
+		
+		if(last[arr[i]] != -1){
+			update(last[arr[i]],-1,n);
+		}else{
+			total++;
+		}
+
+		update(i,1,n);
+		last[arr[i]] = i;
+	
+
+		while(k<q && query[k].second == i){
+			
+			ans[query[k].index] = total - value(query[k].first-1);
+			k++;
+
+		}
+		
+	}
+
+
+	for(int i=0;i<q;i++){
+		cout << ans[i] <<endl;
+	}
+	return 0;
 }
