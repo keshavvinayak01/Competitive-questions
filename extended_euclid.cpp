39c08fbd99589c1b137e9eb9339e498f77f18702
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-08 00:19:57 +0530

    Extended Euclid Algorithm

---
+++ extended_euclid.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Triplet {
+    int x, y, gcd;
+};
+
+Triplet extendedEuclid(int a, int b) {
+    if(b == 0) {
+        Triplet ans;
+        ans.gcd = a;
+        ans.x = 1;
+        ans.y = 0;
+        return ans;
+    }
+    Triplet smallAns = extendedEuclid(b, a%b);
+    Triplet ans;
+    ans.gcd = smallAns.gcd;
+    ans.x = smallAns.y;
+    ans.y = smallAns.x - (a/b)*smallAns.y;
+    return ans;
+}
+
+int main() {
+    int a = 16, b = 10;
+    Triplet ans = extendedEuclid(a,b);
+    cout << ans.x << " " << ans.y << " " << ans.gcd << endl;;
+    return 0;
+}
