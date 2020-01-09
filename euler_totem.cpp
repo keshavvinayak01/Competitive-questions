94e20fdfa341b7c083e0769e75469e77bca6eb09
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-10 01:32:45 +0530

    Euler totem function

---
+++ euler_totem.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+void eulerPhi(int n){
+
+	int phi[n+1];
+
+	for(int i=1;i<=n;i++){
+		phi[i] = i;
+	}
+
+	for(int i=2;i<=n;i++){
+		if(phi[i] == i){
+			phi[i] = i-1;
+			for(int j=2*i;j<=n;j+=i){
+				phi[j] = (phi[j]*(i-1))/i;
+			}
+		}
+	}
+
+	for(int i=1;i<=n;i++){
+		cout << "Euler Totient Phi For " << i << "Is :" << phi[i]<<endl;
+	}
+
+}
+int main(){
+
+	eulerPhi(10);
+	return 0;
+}
