6f7a540b810de216ffa0dad1c0fe31acb28b4efd
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-12 04:29:02 +0530

    Easy : Problem 1

---
+++ codechef/easy/test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    while(true) {
+        int n;
+        cin >> n;
+        if(n == 42) {
+            break;
+        }
+        else cout << n << endl;
+    }
+    return 0;
+}
