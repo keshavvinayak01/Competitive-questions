9910fac388f6c4895a99356356835746eb7f3085
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-02-13 18:38:55 +0530

    Naive lcontest

--- codechef/lcontest_overlap.cpp
+++ codechef/lcontest_overlap.cpp
@@ -4,26 +4,25 @@ typedef long long int lli;
 using namespace std;
 
 bool check_base(lli b, lli base) {
+    base = max(lli(0), base);
     if((b == base*11 + 7) || 
        (b == base*11 + 2) ||
        (b == base*11 + 1))
        return true;
     else return false;
 }
-lli get_ans(lli b, bool first) {
-    lli base = (b)/11;
+lli get_ans(lli b) {
+    lli base = (b - 7)/11;
     lli ans = base*3;
     if(base <= 0) {
         if(b >= 7) ans += 3;
         else if(b == 1) ans += 1;
         else ans += 2;
+        return ans;
     }
     else ans += 3;
-    if(b >= base*11 + 7 && b < base*11 + 12) ans++;
-    else {
-        if(b >= base*11 + 2 && b < base*11 + 7) ans += 2;
-        else if(b >= base*11 + 1 && b < base*11 + 7) ans += 1;
-    }
+    if(b >= base*11 + 2 && b < base*11 + 7) ans += 2;
+    else if(b >= base*11 + 1 && b < base*11 + 7) ans += 1;
     return ans;
 }
 
@@ -34,13 +33,13 @@ int main() {
     while(t--) {
         cin >> a >> b;
         cin >> c >> d;
-        lli ans1 = get_ans(b, true);
-        lli ans2 = get_ans(d, false);
+        lli ans1 = get_ans(b);
+        lli ans2 = get_ans(d);
         lli full_ans = ans2 - ans1;
-        if(check_base(b, b/11)) full_ans++;
-        if(a > 2 && (check_base(b, b/11))) full_ans--;
-        if(c < 2 && (check_base(d, d/11))) full_ans--; 
-        if(full_ans < 0) full_ans = 0;
+        if(check_base(b, (b - 7)/11)) full_ans++;
+        if(a > 2 && (check_base(b, (b - 7)/11))) full_ans--;
+        if(c < 2 && (check_base(d, (d - 7)/11))) full_ans--; 
+
         cout << full_ans << endl;
     }
 
