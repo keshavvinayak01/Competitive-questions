53a92a8479f86fb012b1f7fa67acf74d87aaa39f
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-04-25 10:16:09 +0530

    codechef second

---
+++ codechef/CookOff117B/second.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+typedef long long int lli;
+#define MOD 1000000007
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, r, k;
+        cin >> n >> r >> k;
+        if(r > (n*(n-1))/2) cout << -1 << endl;
+        else {
+
+        }
+    }
+
+    return 0;
+}
