bb6e6fc7f155c21ed5ddb5632407f36a62c42e44
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-04-25 10:15:29 +0530

    testing testing 123

---
+++ codechef/test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+typedef long long int lli;
+#define MOD 1000000007
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);
+    int i = 0;
+    do {
+        cout << "name";
+        i = i++;
+    } while(i < 5);
+    return 0;
+}
