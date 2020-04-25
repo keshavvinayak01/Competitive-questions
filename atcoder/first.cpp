ba04a64392a13db33cc52985989b37af1ac264aa
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-04-25 10:14:45 +0530

    atcoder A

---
+++ atcoder/first.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+typedef long long int lli;
+#define MOD 1000000007
+#define pi 3.1415926
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);
+    double x;
+    cin >> x;
+    cout << 2 * pi * x << endl;
+
+    return 0;
+}
