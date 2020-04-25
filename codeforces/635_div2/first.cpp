6a9a88be00d74efa9efe663fa3dc97dfd539ce23
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-04-25 10:17:02 +0530

    codeforces-635-first

---
+++ codeforces/635_div2/first.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+typedef long long int lli;
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << a << " " << c << " " << c << endl;
+    }
+
+    return 0;
+}
